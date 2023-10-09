27ee87f061f86edcbbd84d3bebe68f0537f0c854
CosmicSpagetti <billyurrutia@gmail.com>
2023-10-09 15:39:03 -0600

    Implemented a new post form

--- vite-project/src/features/posts/PostsList.jsx
+++ vite-project/src/features/posts/PostsList.jsx
@@ -1,41 +1,40 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import { API_URL } from "../../constants";
 
-
 function PostsList() {
-  const [posts, setPosts] = useState([])
-  const [, setLoading] = useState(true)
-  const [, setError] = useState(null)
-  // fetch post 
-  useEffect(() => {
-    async function loadPosts() {
-      try {
-        const response = await fetch(`${API_URL}`);
-        if (response.ok) {
-          const json = await response.json();
-          setPosts(json);
-        } else {
-          throw response
-        }
-      } catch (e) {
-        setError("An error occurred...");
-        console.log("An error occurred:", e)
-      } finally {
-        setLoading(false)
-      }
-    }
-    loadPosts();
-  }, [])
+	const [posts, setPosts] = useState([]);
+	const [, setLoading] = useState(true);
+	const [, setError] = useState(null);
+	// fetch post
+	useEffect(() => {
+		async function loadPosts() {
+			try {
+				const response = await fetch(`${API_URL}`);
+				if (response.ok) {
+					const json = await response.json();
+					setPosts(json);
+				} else {
+					throw response;
+				}
+			} catch (e) {
+				setError("An error occurred...");
+				console.log("An error occurred:", e);
+			} finally {
+				setLoading(false);
+			}
+		}
+		loadPosts();
+	}, []);
 
-  return <div>
-    {posts.map((post) => (
-      <div key={post.id} className="post-container">
-        <h2>{post.title}</h2>
-        <p>
-          {post.body}
-        </p>
-      </div>
-    ))}
-  </div>
+	return (
+		<div>
+			{posts.map((post) => (
+				<div key={post.id} className="post-container">
+					<h2>{post.title}</h2>
+					<p>{post.body}</p>
+				</div>
+			))}
+		</div>
+	);
 }
 export default PostsList;
